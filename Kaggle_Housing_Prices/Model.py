3a1e584530f1686ab7b8fcf1c6cd1e8c3c8b1855
khteka <khteka@gmail.com>
2017-12-16 22:25:27 -0800

    Update Model.py

--- Kaggle_Housing_Prices/Model.py
+++ Kaggle_Housing_Prices/Model.py
@@ -1,8 +1,15 @@
-Steps we will go through in this project:
-1. Look at the bigger picture.
-2. Get the data.
-3. Discover and visualize the data to gain insights.
-4. Prepare the data for Machine Learning algorithms.
-5. Select a model and train it.
-6. Fine-tune model.
-7. Submit solution.
+# Steps we will go through in this project:
+# 1. Look at the bigger picture.
+# 2. Get the data.
+# 3. Discover and visualize the data to gain insights.
+# 4. Prepare the data for Machine Learning algorithms.
+# 5. Select a model and train it.
+# 6. Fine-tune model.
+# 7. Submit solution.
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+
